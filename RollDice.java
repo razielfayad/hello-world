321775911e79024cc75f976ab0c18e593bf5645c
Jackh4830 <jackh.4830@gmail.com>
2016-10-05 21:39:44 -0500

    Change random

--- RollDice.java
+++ RollDice.java
@@ -5,7 +5,7 @@ public class RollDice {
 	public static void main(String[] args) {
 		Random dice = new Random();
 		int number;	
-	    	number = 1 + dice.nextInt(6);
+	    	number = 25 * dice.nextInt(6);
 		System.out.println(number);
 	}
 
